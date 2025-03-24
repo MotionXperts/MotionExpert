f9f146ae3938007bde711b657f82816f12a6d88d
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-03-24 15:40:59 +0800

    Enhance coding style and improve readability
    Ensure the coding style of STAGCN.py in the models folder is consistent
    with the rest of the project.
    
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- models/STAGCN.py
+++ models/STAGCN.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-###### STA-GCN ######
+# STA-GCN
 from net.Utils_attention.attention_branch import *
 from net.Utils_attention.perception_branch import *
 from net.Utils_attention.feature_extractor import *
@@ -9,54 +9,52 @@ from net.Utils_attention.graph_convolution import *
 from net.Utils_attention.make_graph import Graph
 
 class STA_GCN(nn.Module):
-    def __init__(self, num_class, in_channels, residual, dropout, 
-                 t_kernel_size, layout, strategy, hop_size, num_att_A, PRETRAIN_SETTING, PRETRAIN = True, lora_config = None):
+    def __init__(self, num_class, in_channels, residual, dropout,
+                 t_kernel_size, layout, strategy, hop_size, num_att_A,
+                 PRETRAIN_SETTING, PRETRAIN = True, lora_config = None) :
         super().__init__()
         self.PRETRAIN_SETTING = PRETRAIN_SETTING
         self.PRETRAIN = PRETRAIN
         # Graph
-        graph = Graph(layout=layout, strategy=strategy, hop_size=hop_size)
-        A = torch.tensor(graph.A, dtype=torch.float32, requires_grad=False)
+        graph = Graph(layout=layout, strategy = strategy, hop_size = hop_size)
+        A = torch.tensor(graph.A, dtype = torch.float32, requires_grad = False)
         self.register_buffer('A', A)
 
         # config
-        kwargs = dict(s_kernel_size=A.size(0),
-                      t_kernel_size=t_kernel_size,
-                      dropout=dropout,
-                      residual=residual,
-                      A_size=A.size(),
+        kwargs = dict(s_kernel_size = A.size(0),
+                      t_kernel_size = t_kernel_size,
+                      dropout = dropout,
+                      residual = residual,
+                      A_size = A.size(),
                       PRETRAIN_SETTING = self.PRETRAIN_SETTING,
                       PRETRAIN = self.PRETRAIN,
                       lora_config = lora_config)
 
-        
         if self.PRETRAIN_SETTING == 'STAGCN' :
-            f_config = [[in_channels, 32, 1],   [ 32,  32, 1],  [ 32,  32, 1],  [ 32, 64, 2],   [ 64,  64, 1]]
-            a_config = [[128, 128, 1],          [128, 128, 1],  [128, 256, 2],  [256, 256, 1],  [256, 256, 1]]
-            p_config = [[128, 128, 1],          [128, 128, 1],  [128, 256, 2],  [256, 256, 1],  [256, 256, 1]]
+            f_config = [[in_channels, 32, 1], [32, 32, 1], [32, 32, 1], [32, 64, 2], [64, 64, 1]]
+            a_config = [[128, 128, 1], [128, 128, 1], [128, 256, 2], [256, 256, 1], [256, 256, 1]]
+            p_config = [[128, 128, 1], [128, 128, 1], [128, 256, 2], [256, 256, 1], [256, 256, 1]]
 
         # PRETRAIN_SETTING : 'Attention'
-        else : 
-            f_config = [[in_channels, 32, 1],   [ 32,  32, 1],  [ 32, 64, 1],   [ 64, 64, 1],    [64, 128, 1], 
-                        [128, 128, 1],          [128, 128, 1],  [128, 256, 1],  [256, 256, 1],  [256, 256, 1]]
+        else :
+            f_config = [[in_channels, 32, 1], [32, 32, 1], [32, 64, 1], [64, 64, 1], [64, 128, 1],
+                        [128, 128, 1], [128, 128, 1], [128, 256, 1], [256, 256, 1], [256, 256, 1]]
             a_config = [[256, 256, 1]]
-            p_config = [[128, 128, 1],          [128, 128, 1],  [128, 256, 1],  [256, 256, 1],  [256, 256, 1]]
+            p_config = [[128, 128, 1], [128, 128, 1], [128, 256, 1], [256, 256, 1], [256, 256, 1]]
 
-        self.feature_extractor = Feature_extractor(config=f_config, **kwargs)
-        self.attention_branch = Attention_branch(config=a_config,num_class=num_class, num_att_A=num_att_A, **kwargs)
-        self.perception_branch = Perception_branch(config=p_config,num_class=num_class, num_att_A=num_att_A, **kwargs)
+        self.feature_extractor = Feature_extractor(config = f_config, **kwargs)
+        self.attention_branch = Attention_branch(config = a_config, num_class = num_class, num_att_A = num_att_A, **kwargs)
+        self.perception_branch = Perception_branch(config = p_config, num_class = num_class, num_att_A = num_att_A, **kwargs)
         self.output_channel = p_config[-1][1] + a_config[-1][1] 
 
     def forward(self, x):
         # [N : number of attention, c : channels, t : number of frame, v : joints]
-        N, c, t, v = x.size() 
- 
         if self.PRETRAIN_SETTING == 'STAGCN' :
             feature = self.feature_extractor(x, self.A)
-            attention_last , att_node, att_A = self.attention_branch(feature, self.A)
+            attention_last, att_node, att_A = self.attention_branch(feature, self.A)
 
         else :
-            feature,feature_last = self.feature_extractor(x, self.A)
+            feature, feature_last = self.feature_extractor(x, self.A)
             att_node, att_A = self.attention_branch(feature_last, self.A)
 
         # Attention Mechanism
@@ -71,12 +69,12 @@ class STA_GCN(nn.Module):
             Convert to [batchsize, number of frame, joints(22), channel(256)]
             Concatenate two embeding to [batchsize, number of frame, joints(22), channel(512)]
         '''
-        perception_last = perception_last.permute(0,2,3,1)  
+        perception_last = perception_last.permute(0, 2, 3, 1)
         if self.PRETRAIN_SETTING == 'STAGCN' :
-            attention_last = attention_last.permute(0,2,3,1) 
-            concatenate_embedding = torch.cat([perception_last, attention_last], dim=-1)
+            attention_last = attention_last.permute(0, 2, 3, 1)
+            concatenate_embedding = torch.cat([perception_last, attention_last], dim = -1)
         else :
-            feature_last   = feature_last.permute(0,2,3,1)     
-            concatenate_embedding = torch.cat([perception_last, feature_last], dim=-1) 
+            feature_last   = feature_last.permute(0, 2, 3, 1)
+            concatenate_embedding = torch.cat([perception_last, feature_last], dim = -1)
 
         return concatenate_embedding, att_node, att_A
