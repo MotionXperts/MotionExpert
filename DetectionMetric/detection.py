8e0d42cd110658a91f74389bf234c0848e1af013
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-04-29 21:03:20 +0800

    Add six indicators for sport utility evaluation
    Introuduce a set of evaluation indicators to assess the sport utility of
    the generated instructions. There are six indicators "Error Detection",
    "Time Detection", "Body Part Detection", "Causation", "Method" and
    "Coordination".
    
    In every evaluation file, we evaluate G-eval score on every instruction
    and record their G-eval score in according json file.
    
    In statics.py, we calculate the number of guidance prompts in each
    indicator's G-eval and calculate the relationship between two
    indicators, counting it as one occurrence when both indicators appear
    together in the same instruction, which are presented as an association
    matrix.
    
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

---
+++ DetectionMetric/detection.py
@@ -0,0 +1,70 @@
+import json, argparse, tqdm, time, os, re
+import anthropic
+def read_template(prompt_fp):
+    text_template = open(prompt_fp).read()
+    return text_template
+
+def read_data(predict_path):   
+    with open(predict_path, 'r', encoding='utf-8') as f:
+        predictions = json.load(f)
+
+    return predictions
+
+def acc(args, filename, metric):
+    output_filename = metric + '_Detection_{{filename}}'
+
+    output_filename = output_filename.replace('{{filename}}',filename)
+    output_filepath = os.path.join(args.output,output_filename)
+    with open(output_filepath, 'r') as f:
+        datas = json.load(f)
+
+    acc_shot_count = 0
+    acc_score = 0
+
+    score_name = metric + '_Detection_score'
+    for data in datas:
+        if data[score_name] != 0 and data["score"] > 1:
+            acc_shot_count += 1
+            acc_score += data["score"]
+    return acc_score, acc_shot_count
+
+def g_eval(args,summeval, prompt, api_key, filename, metric):
+    new_json = []
+    count, ignore, all_score = 0, 0, 0
+
+    score_name = metric + '_Detection_score'
+    prompt_name = metric + '_Detection_prompt'
+
+    for instance in tqdm.tqdm(summeval):
+        system_output   = instance['system_output']
+        cur_prompt      = prompt.replace('{{Instruction}}', system_output)
+        instance[prompt_name] = cur_prompt
+        
+        client = anthropic.Anthropic(api_key=api_key)
+        try:
+            _response   = client.messages.create(model="claude-3-5-sonnet-20240620",
+                                                 messages=[{"role": "user", "content": cur_prompt}],
+                                                 max_tokens=5,)
+            content     = _response.content
+            print(f"Claude response: {content}")  # 檢查 Claude 回應的完整內容
+
+            text_value  = content[0].text
+            match       = re.search(r'\d+', text_value)
+            instance[score_name] = int(match.group())
+            
+            new_json.append(instance)
+
+            count += 1
+            all_score += instance[score_name]
+
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            ignore += 1
+    
+    output_filename = metric + '_Detection_{{filename}}'
+    output_filename = output_filename.replace('{{filename}}',filename)
+
+    output_filepath = os.path.join(args.output,output_filename)
+    with open(output_filepath, 'w') as f:
+        json.dump(new_json, f, indent=4)
+    return all_score/count, all_score
---
+++ DetectionMetric/statics.py
@@ -0,0 +1,144 @@
+import json
+def read_data(predict_path):   
+    with open(predict_path, 'r', encoding='utf-8') as f:
+        predictions = json.load(f)
+
+    return predictions
+body_part_file      = "./BodyPart_Detection_geval_epoch_135.json"
+causation_file      = "./Causation_Detection_geval_epoch_135.json"
+coordination_file   = "./Coordination_Detection_geval_epoch_135.json"
+error_file          = "./Error_Detection_geval_epoch_135.json"
+method_file         = "./Method_Detection_geval_epoch_135.json"
+time_file           = "./Time_Detection_geval_epoch_135.json"
+
+body_part_file      = "./BodyPart_Detection_geval_instruction_by_GPT4-o"
+causation_file      = "./Causation_Detection_geval_instruction_by_GPT4-o"
+coordination_file   = "./Coordination_Detection_geval_instruction_by_GPT4-o"
+error_file          = "./Error_Detection_geval_instruction_by_GPT4-o"
+method_file         = "./Method_Detection_geval_instruction_by_GPT4-o"
+time_file           = "./Time_Detection_geval_instruction_by_GPT4-o"
+
+body_part_file      = "./BodyPart_Detection_geval_llama32_skating"
+causation_file      = "./Causation_Detection_geval_llama32_skating"
+coordination_file   = "./Coordination_Detection_geval_llama32_skating"
+error_file          = "./Error_Detection_geval_llama32_skating"
+method_file         = "./Method_Detection_geval_llama32_skating"
+time_file           = "./Time_Detection_geval_llama32_skating"
+
+body_part_file      = "./BodyPart_Detection_geval_boxing_epoch70"
+causation_file      = "./Causation_Detection_geval_boxing_epoch70"
+coordination_file   = "./Coordination_Detection_geval_boxing_epoch70"
+error_file          = "./Error_Detection_geval_boxing_epoch70"
+method_file         = "./Method_Detection_geval_boxing_epoch70"
+time_file           = "./Time_Detection_geval_boxing_epoch70"
+
+if __name__ == '__main__':
+    body_part = read_data(body_part_file)
+    causation = read_data(causation_file)
+    coordination = read_data(coordination_file)
+    error = read_data(error_file)
+    method = read_data(method_file)
+    time = read_data(time_file)
+
+    metric1 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    metric2 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    metric3 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    metric4 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    metric5 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    metric6 = { "total": 0, "score_1" : 0, "score_2" : 0, "score_3" : 0, "score_4" : 0, "score_5" : 0}
+    
+    all_instructions = {}
+    for item in body_part:
+        all_instructions[item["file_name"]] = { 
+            "score" : item["score"],
+            "1" :item["BodyPart_Detection_score"]}
+
+        if item["BodyPart_Detection_score"] == 1 :
+            metric3["total"] += 1
+            if item["score"] == 1: metric3["score_1"] += 1
+            elif item["score"] == 2: metric3["score_2"] += 1
+            elif item["score"] == 3: metric3["score_3"] += 1
+            elif item["score"] == 4: metric3["score_4"] += 1
+            elif item["score"] == 5: metric3["score_5"] += 1
+
+    for item in causation:
+        all_instructions[item["file_name"]]["2"] = item["Causation_Detection_score"]
+        if item["Causation_Detection_score"] == 1 :
+            metric4["total"] += 1
+            if item["score"] == 1: metric4["score_1"] += 1
+            elif item["score"] == 2: metric4["score_2"] += 1
+            elif item["score"] == 3: metric4["score_3"] += 1
+            elif item["score"] == 4: metric4["score_4"] += 1
+            elif item["score"] == 5: metric4["score_5"] += 1
+
+    for item in coordination:
+        all_instructions[item["file_name"]]["3"] = item["Coordination_Detection_score"]
+        if item["Coordination_Detection_score"] == 1 :
+            metric6["total"] += 1
+            if item["score"] == 1: metric6["score_1"] += 1
+            elif item["score"] == 2: metric6["score_2"] += 1
+            elif item["score"] == 3: metric6["score_3"] += 1
+            elif item["score"] == 4: metric6["score_4"] += 1
+            elif item["score"] == 5: metric6["score_5"] += 1
+
+    for item in error:
+        all_instructions[item["file_name"]]["4"] = item["Error_Detection_score"]
+        if item["Error_Detection_score"] == 1 :
+            metric1["total"] += 1
+            if item["score"] == 1: metric1["score_1"] += 1
+            elif item["score"] == 2: metric1["score_2"] += 1
+            elif item["score"] == 3: metric1["score_3"] += 1
+            elif item["score"] == 4: metric1["score_4"] += 1
+            elif item["score"] == 5: metric1["score_5"] += 1
+
+    for item in method:
+        all_instructions[item["file_name"]]["5"] = item["Method_Detection_score"]
+        if item["Method_Detection_score"] == 1 :
+            metric5["total"] += 1
+            if item["score"] == 1: metric5["score_1"] += 1
+            elif item["score"] == 2: metric5["score_2"] += 1
+            elif item["score"] == 3: metric5["score_3"] += 1
+            elif item["score"] == 4: metric5["score_4"] += 1
+            elif item["score"] == 5: metric5["score_5"] += 1
+    
+    for item in time:
+        all_instructions[item["file_name"]]["6"] = item["Time_Detection_score"]
+        if item["Time_Detection_score"] == 1 :
+            metric2["total"] += 1
+            if item["score"] == 1: metric2["score_1"] += 1
+            elif item["score"] == 2: metric2["score_2"] += 1
+            elif item["score"] == 3: metric2["score_3"] += 1
+            elif item["score"] == 4: metric2["score_4"] += 1
+            elif item["score"] == 5: metric2["score_5"] += 1
+
+    matrix = [[0] * 6 for _ in range(6)]  # 建立 6x6 矩陣
+
+    for i in range(6):
+        for j in range(6):
+            if i == j: continue
+            for item in all_instructions:
+                data = all_instructions[item]
+                key1 = str(i + 1)  # 把 i+1 轉成字串
+                key2 = str(j + 1)  # 把 j+1 轉成字串
+                print(data)
+                if key1 in data and key2 in data:  # 確保 key 存在於字典
+                    if data[key1] > 0 and data[key2] > 0 and data["score"] > 1:
+                        matrix[i][j] += data["score"]
+                        matrix[j][i] += data["score"]
+
+    data = {
+        "metric1": metric1,
+        "metric2": metric2,
+        "metric3": metric3,
+        "metric4": metric4,
+        "metric5": metric5,
+        "metric6": metric6,
+        "matrix" : matrix
+    }       
+    output_filepath = "analyze_boxing.json"
+    with open(output_filepath, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    
+
+                
