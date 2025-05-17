b7f2db73f9063afb6a87a919f7fb56e66ba76c8c
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-05-18 07:03:18 +0800

    gt_preprocess: add utility clean_instruction file
    Add the function 'clean_instruction' to sanitize instruction strings and
    filter out common formatting issues that frequently appear.
    
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

---
+++ gt_preprocess/clean_instruction.py
@@ -0,0 +1,18 @@
+def clean_instruction(text):
+    text = text.rstrip()
+    if text.startswith(' '):
+        text = text.lstrip()
+    text = re.sub(r'\s+', ' ', text)
+    if not text.endswith('.'):
+        text += '.'
+
+    if "instruction: " in text:
+        print("Invalid text: ",text)
+        text = text.split("instruction: ")[1]
+        print("Valid text: ",text)
+
+    if "Instruction: " in text:
+        print("Invalid text: ",text)
+        text = text.split("Instruction: ")[1]
+        print("Valid text: ",text)
+    return text
