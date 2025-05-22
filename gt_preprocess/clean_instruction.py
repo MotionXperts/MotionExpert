8e6a215bb375550146df214d46b10c95aa81f9c2
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-05-23 02:05:14 +0800

    gt_preprocess: add rules for cleaning instruction
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- gt_preprocess/clean_instruction.py
+++ gt_preprocess/clean_instruction.py
@@ -7,6 +7,41 @@ def clean_instruction(text):
     if not text.endswith('.'):
         text += '.'
 
+    if "The instruction is: " in text:
+        print("Invalid text: ",text)
+        text = text.split("The instruction is: ")[1]
+        print("Valid text: ",text)
+
+    if "The target instruction means" in text:
+        print("Invalid text: ",text)
+        text = text.split("The target instruction means")[1]
+        print("Valid text: ",text)
+
+    if "The instruction translates to:" in text:
+        print("Invalid text: ",text)
+        text = text.split("The instruction translates to: ")[1]
+        print("Valid text: ",text)
+
+    if "The instruction in English would be:" in text:
+        print("Invalid text: ",text)
+        text = text.split("The instruction in English would be: ")[1]
+        print("Valid text: ",text)
+
+    if "Instruction for the target:" in text:
+        print("Invalid text: ",text)
+        text = text.split("target: ")[1]
+        print("Valid text: ",text)
+
+    if "Instruction translation: " in text:
+        print("Invalid text: ",text)
+        text = text.split("Instruction translation: ")[1]
+        print("Valid text: ",text)
+    
+    if "Translation: " in text:
+        print("Invalid text: ",text)
+        text = text.split("Translation: ")[1]
+        print("Valid text: ",text)
+
     if "instruction: " in text:
         print("Invalid text: ",text)
         text = text.split("instruction: ")[1]
