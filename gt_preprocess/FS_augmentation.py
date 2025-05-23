26843cb182d63be5abc9e3aa64f961143d7c0fab
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-05-23 14:02:20 +0800

    gt_preprocess: change to Cindy's OpenAI setting
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- gt_preprocess/FS_augmentation.py
+++ gt_preprocess/FS_augmentation.py
@@ -15,7 +15,7 @@ def FS_augmentation(instruction, motion_type, index):
     load_dotenv(dotenv_path=env_path)
     openai.api_key = os.getenv("CINDY_OPENAI_KEY")
     openai.organization = os.getenv("CINDY_ORGANIZATION")
-
+    # openai.api_key = os.getenv("ALLEN_OPENAI_KEY")
     print("Running augmentation...")
 
     system_prompt = FS_get_system_prompt(motion_type, index + 1)
@@ -23,6 +23,7 @@ def FS_augmentation(instruction, motion_type, index):
 
     response = openai.chat.completions.create(
         model="gpt-4o-2024-08-06",
+        # model="gpt-4o",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": f'Target instruction: {instruction}'}
