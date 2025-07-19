8ba1a9e45407244d913dd401d6b155f6bcd428da
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-07-19 12:34:08 +0800

    Add configurable paths, and FPS for GIF generation
    Add usage comment for clarity.
    Use new images_directory and FPS variables for different sports.
    
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- utils/image_to_gif.py
+++ utils/image_to_gif.py
@@ -1,26 +1,35 @@
 import os
 from PIL import Image
 
-image_folder = 'dir'
-output_gif_path = 'output.gif'        
+# Usage : Specify the images_directory for the images, the path where the GIF will be saved,
+#         and the FPS (frames per second).
 
+# Boxing
+images_directory = '/home/weihsin/projects/HybrIK/Boxing/cam3/4_front_1/raw_images'
+output_gif_path = '/home/weihsin/projects/HybrIK/Boxing/cam3/4_front_1/output.gif'
+FPS = 59
+
+# Figure Skating
+images_directory = '/home/weihsin/projects/HybrIK/Output/471706290155159700_0/raw_images'
+output_gif_path = '/home/weihsin/projects/HybrIK/Output/471706290155159700_0/output.gif'
+FPS = 30
 image_files = sorted([
-    file for file in os.listdir(image_folder)
+    file for file in os.listdir(images_directory)
     if file.lower().endswith(('.png', '.jpg', '.jpeg'))
 ])
 
 if not image_files:
     exit()
 
-first_image = Image.open(os.path.join(image_folder, image_files[0]))
+first_image = Image.open(os.path.join(images_directory, image_files[0]))
 
-frames = [Image.open(os.path.join(image_folder, img)) for img in image_files[1:]]
+frames = [Image.open(os.path.join(images_directory, img)) for img in image_files[1:]]
 
 first_image.save(
     output_gif_path,
     format='GIF',
     save_all=True,
     append_images=frames,
-    fps=60,
+    fps=FPS,
     loop=0         
 )
