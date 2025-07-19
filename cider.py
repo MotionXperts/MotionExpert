72e898a96bd529c1427d43c7a5d15c425c01d1c6
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-07-19 13:27:13 +0800

    Remove evaluation code
    Remove evaluation code including main function and argument parsing.
    
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- cider.py
+++ cider.py
@@ -58,34 +58,3 @@ class CIDERScore:
             answers.append(gts[img_name][0] if isinstance(gts[img_name], list) else gts[img_name])
         results = self.evaluator.run_evaluation(predicts, answers)
         return results['CIDEr']
-
-def main(args):
-    # Read data
-    predictions = readJSON(args.pred_file)
-    annotations = readPickle(args.annotation_file)
-
-    # Preprocess annotation file
-    gts = getGTCaptions(annotations)
-
-    # Check predictions content is correct
-    assert type(predictions) is dict
-    assert set(predictions.keys()) == set(gts.keys())
-    assert all([type(pred) is str for pred in predictions.values()])
-
-    # CIDErScore
-    cider_score = CIDERScore()(predictions, gts)
-    bleu_score = BLEUScore()(predictions, gts)
-
-    print(f"CIDEr: {cider_score}")
-    print(f"BLEU: {bleu_score}")    
-
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-
-    parser.add_argument("--pred_file"       ,default='/home/peihsin/projects/MotionExpert/results_epoch15.json',    help="Prediction json file")
-    parser.add_argument("--annotation_file" ,default="/home/peihsin/projects/humanML/dataset/rm_test.pkl",          help="Annotation json file")
-
-    args = parser.parse_args()
-
-    main(args)
