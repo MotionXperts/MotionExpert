06c26a56ee371994db9240693b058d75d08f52b8
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2025-09-16 13:02:57 +0800

    fix(dataset): import missing random module
    Signed-off-by: Wei-Hsin Yeh <weihsinyeh168@gmail.com>

--- dataloaders/Dataset.py
+++ dataloaders/Dataset.py
@@ -1,6 +1,6 @@
 import pickle, os, json, torch, numpy as np
 from torch.utils.data import Dataset
-
+import random
 # The direction of the bone.
 bonelink = [(0, 1), (0, 2), (0, 3), (1, 4), (2, 5), (3, 6), (4, 7), (5, 8), (6, 9), (7, 10), (8, 11),
             (9, 12), (9, 13), (9, 14), (12, 15), (13, 16), (14, 17), (16, 18), (17, 19), (18, 20), (19, 21)]
